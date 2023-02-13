7fccfb4110e4ccb4e667a18a6a5e5bcfadccea63
Mauricio Farias <mauricio.macario@palmsoft.com.br>
2023-02-13 17:10:01 -0300

    Adicionado setar partições manualmente

--- str-consumer/src/main/java/com/mauricio/strconsumer/listeners/StrConsumerListerner.java
+++ str-consumer/src/main/java/com/mauricio/strconsumer/listeners/StrConsumerListerner.java
@@ -1,6 +1,7 @@
 package com.mauricio.strconsumer.listeners;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j2;
@@ -9,12 +10,20 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class StrConsumerListerner {
 	
-	@KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-1", 
+			topicPartitions = { 
+					@TopicPartition(topic = "str-topic", partitions = {"0"}) //OPCIONAL, CASO NÃO SETADO A PARTITION O KAFKA DIRECIONA AUTOMATICAMENTE
+			},
+			containerFactory = "strContainerFactory")
 	public void listener(String message) {
 		log.info("CREATE ::: Receive message {}", message);
 	}
 	
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-1", 
+			topicPartitions = {
+					@TopicPartition(topic = "str-topic", partitions = {"1"})
+			}, 
+			containerFactory = "strContainerFactory")
 	public void log(String message) {
 		log.info("LOG ::: Receive message {}", message);
 	}
