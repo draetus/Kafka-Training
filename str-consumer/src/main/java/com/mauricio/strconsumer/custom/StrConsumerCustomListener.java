9a5decb4b2198e78a47428e37758fe6095d1249d
Mauricio Farias <mauricio.macario@palmsoft.com.br>
2023-02-15 11:26:36 -0300

    Adicionado error handler

--- str-consumer/src/main/java/com/mauricio/strconsumer/custom/StrConsumerCustomListener.java
+++ str-consumer/src/main/java/com/mauricio/strconsumer/custom/StrConsumerCustomListener.java
@@ -21,5 +21,8 @@ public @interface StrConsumerCustomListener {
 	
 	@AliasFor(annotation = KafkaListener.class, attribute = "groupId") 
 	String groupId() default "";
+	
+	@AliasFor(annotation = KafkaListener.class, attribute = "errorHandler") 
+	String errorHandler() default "errorCustomHandler";
 
 }
