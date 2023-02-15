0ac76059a492abcf7d4429d7053ad316a33790d0
Mauricio Farias <mauricio.macario@palmsoft.com.br>
2023-02-15 11:34:01 -0300

    Quick fix

--- str-consumer/src/main/java/com/mauricio/strconsumer/exceptions/ErrorCustomHandler.java
+++ str-consumer/src/main/java/com/mauricio/strconsumer/exceptions/ErrorCustomHandler.java
@@ -14,13 +14,10 @@ public class ErrorCustomHandler implements KafkaListenerErrorHandler {
 	@Override
 	public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
 		log.info("EXCEPTION_HANDLER ::: Capturei um erro");
-<<<<<<< HEAD
 		log.info("Payload ::: {}", message.getPayload());
 		log.info("Headers ::: {}", message.getHeaders());
 		log.info("Offset ::: {}", message.getHeaders().get("kafka_offset"));
 		log.info("Message Exception ::: {}", exception.getMessage());
-=======
->>>>>>> 8ff71e1a4aa1f0c9e6b5333e3a5292e8682e8a53
 		return null;
 	}
 	
